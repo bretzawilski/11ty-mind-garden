c94b836c462a2341585711919ed356699192af22
Bret Zawilski <bret.zawilski@gmail.com>
2022-06-07 13:47:03 +0100

    Layouts Rough Complete

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  
+  // Set custom directories for input, output, includes, and data
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+layout: layout.njk
+title: "Index"
+fileName: index
+---
+
+# This is the Start
+Thanks all! I'm so glad that you're here with us!n
+
